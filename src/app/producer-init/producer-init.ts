6bcf4b4d9d53dd8fe453c6a0e0e8ec765dc56782
Marc Stroebel <m.stroebel@gmx.de>
2025-09-15 18:31:09 +0200

    spinner style

--- src/app/producer-init/producer-init.ts
+++ src/app/producer-init/producer-init.ts
@@ -1,31 +1,31 @@
-import {ChangeDetectionStrategy, Component, effect, inject, output} from '@angular/core';
-import {toSignal} from '@angular/core/rxjs-interop';
-import {from} from 'rxjs';
-import {ProducerDataService} from '../producer-data.service';
-import {DbService} from '../db.service';
+import { ChangeDetectionStrategy, Component, effect, inject, output } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { from } from 'rxjs';
+import { ProducerDataService } from '../producer-data.service';
+import { DbService } from '../db.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-producer-init',
-  imports: [],
+  imports: [MatProgressSpinnerModule],
   templateUrl: './producer-init.html',
   styleUrl: './producer-init.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProducerInit {
-
   private readonly _producerDataService = inject(ProducerDataService);
   private readonly _dbService = inject(DbService);
 
-  producers =  toSignal(from(this._producerDataService.getProducerData()));
+  producers = toSignal(from(this._producerDataService.getProducerData()));
 
-  dataReady = output<boolean>()
+  dataReady = output<boolean>();
 
   constructor() {
     effect(() => {
-      if(this.producers()) {
-        if(this._dbService.dbReady()) {
+      if (this.producers()) {
+        if (this._dbService.dbReady()) {
           const result = this._dbService.importData(this.producers() ?? []);
-          if(result.importComplete()) {
+          if (result.importComplete()) {
             this.dataReady.emit(true);
           }
         }
